88a54c6dbfa4ca189dab26890593bc02b667907a
Gleb <37934662+quanuhs@users.noreply.github.com>
2020-06-14 22:16:45 +0300

    Authorization responce (only in console)
    Добавил в login.kv: 
    1. 'id' для текстовых полей 'login' и 'password'.
    2. Отработка события "on_press" для кнопки.
    Соединил необходимое с main.py (передача данных с текстовых полей), 
    а также добавил в main.py функцию авторизации в ЛК Бонча.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from kivy.app import App
-from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivymd.app import MDApp
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+
+
+
+
+s = requests.Session()
+s.get("https://lk.sut.ru/cabinet/")
+
+def login_bonch(login, password):
+    data = {'users': login,
+            'parole': password}
+
+    r = s.post("https://lk.sut.ru/cabinet/lib/autentificationok.php", data = data)
+
+    logged = r.text
+    if '1' == logged:
+        print("Подключено через Email: %s.." % login)
+    else:
+        print("Не получилось подключиться..\nВозможно вы ввели неверный логин или пароль.")
+
+
 
 from kivy.core.window import Window
 Window.size = (480, 850)
@@ -12,12 +35,14 @@ from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
 class LoginForm(BoxLayout):
-    pass
+    def page_auth_get_data(self):
+        login_bonch(self.login.text, self.password.text)
 
 class LoginApp(MDApp):
     tittle = 'lkBonchApp'
 
     def build(self):
+        self.theme_cls.theme_style = 'Light'
         return LoginForm()
 
 if __name__ == '__main__':

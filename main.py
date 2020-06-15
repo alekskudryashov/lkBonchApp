c764e8743c75d809647e938efc3c79409b75be16
Aleks Kudryashov <aleks59niki30@gmail.com>
2020-06-15 14:36:59 +0300

    Some changes

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
-from kivy.lang import Builder
 from kivy.app import App
-from kivy.properties import ObjectProperty
 from kivymd.app import MDApp
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-import requests
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+
 from bs4 import BeautifulSoup
-import re
-import time
-
-
-
-
-s = requests.Session()
-s.get("https://lk.sut.ru/cabinet/")
-
-def login_bonch(login, password):
-    data = {'users': login,
-            'parole': password}
-
-    r = s.post("https://lk.sut.ru/cabinet/lib/autentificationok.php", data = data)
-
-    logged = r.text
-    if '1' == logged:
-        print("Подключено через Email: %s.." % login)
-    else:
-        print("Не получилось подключиться..\nВозможно вы ввели неверный логин или пароль.")
-
-
+import requests
 
 from kivy.core.window import Window
 Window.size = (480, 850)
@@ -34,15 +14,32 @@ Window.size = (480, 850)
 from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
+session = requests.Session()
+session.get('https://lk.sut.ru/cabinet/')
+
+def UserAuthorization(login, password):
+    data = {'users': login,
+            'parple': password}
+
+    response = session.post('https://lk.sut.ru/cabinet/lib/autentificationok.php', data = data)
+    isLogged = response.text
+
+    return isLogged
+
 class LoginForm(BoxLayout):
-    def page_auth_get_data(self):
-        login_bonch(self.login.text, self.password.text)
+    def getLoginData(self):
+        if UserAuthorization(self.login.text, self.password.text) == '1':
+            print('Okey')
+        else:
+            print('Fuck')
+            self.ids.password.error = True
+
+
 
 class LoginApp(MDApp):
     tittle = 'lkBonchApp'
 
     def build(self):
-        self.theme_cls.theme_style = 'Light'
         return LoginForm()
 
 if __name__ == '__main__':

b262b64a5162bc936a78b1b88ddcfb703dda6abf
Gleb <37934662+quanuhs@users.noreply.github.com>
2020-06-15 15:07:51 +0300

    Updated main.py | Fixed auth.

--- main.py
+++ main.py
@@ -4,40 +4,40 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
-
+from kivy.core.window import Window
+from kivy.config import Config
 from bs4 import BeautifulSoup
 import requests
 
-from kivy.core.window import Window
-Window.size = (480, 850)
-
-from kivy.config import Config
-Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
 session = requests.Session()
 session.get('https://lk.sut.ru/cabinet/')
 
+
 def UserAuthorization(login, password):
     data = {'users': login,
-            'parple': password}
+            'parole': password}
 
-    response = session.post('https://lk.sut.ru/cabinet/lib/autentificationok.php', data = data)
-    isLogged = response.text
+    r = session.post("https://lk.sut.ru/cabinet/lib/autentificationok.php", data=data)
+    return r.text
 
-    return isLogged
 
 class LoginForm(BoxLayout):
     def getLoginData(self):
-        if UserAuthorization(self.login.text, self.password.text) == '1':
+        auth = UserAuthorization(self.login.text, self.password.text)
+        if auth == '1':
             print('Okey')
-        else:
+        elif auth == '0':
             print('Fuck')
             self.ids.password.error = True
-
+        else:
+            print("Well, shit.. here we go again. Bonch rip.")
 
 
 class LoginApp(MDApp):
     tittle = 'lkBonchApp'
+    Window.size = (480, 850)
+    Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
     def build(self):
         return LoginForm()
